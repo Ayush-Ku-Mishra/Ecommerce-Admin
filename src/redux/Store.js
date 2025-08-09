f5fde3914354ffe39feed8ebea9688fc588d2e76
1st-demo <amishra59137@gmail.com>
2025-08-10 01:33:41 +0530

    Fixing again store.js

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,21 +1,11 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
 
-// Create a dummy slice just to avoid empty reducer errors
-const dummySlice = createSlice({
-  name: 'dummy',
-  initialState: { message: 'This is a dummy reducer' },
-  reducers: {
-    setMessage: (state, action) => {
-      state.message = action.payload;
-    }
-  }
-});
-
-export const { setMessage } = dummySlice.actions;
+const dummyReducer = (state = {}, action) => state;
 
 export const store = configureStore({
   reducer: {
-    dummy: dummySlice.reducer
+    dummy: dummyReducer
   }
 });
 
